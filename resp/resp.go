6d610ff6280ee862c2c69d03f3ee819dc7d18aad
devitodbug <davidochiengy@gmail.com>
2026-04-05 00:20:43 +0300

    feat: add Read, readArray, and readBulk methods to RESP for handling array and bulk types

--- resp/resp.go
+++ resp/resp.go
@@ -4,6 +4,7 @@ package resp
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"strconv"
 )
@@ -15,6 +16,23 @@ type Resp struct {
 func NewResp(rd io.Reader) *Resp {
 	return &Resp{
 		reader: bufio.NewReader(rd),
+	}
+}
+
+func (r *Resp) Read() (*Value, error) {
+	_type, err := r.reader.ReadByte() // first character should tell the type refer to types.go
+	if err != nil {
+		return nil, nil
+	}
+
+	switch _type {
+	case ARRAY:
+		return r.readArray()
+	case BULK:
+		return r.readBulk()
+
+	default:
+		return nil, fmt.Errorf("invalid input type")
 	}
 }
 
@@ -61,4 +79,58 @@ func (r *Resp) readInt() (val int, bytesConsumed int, err error) {
 	}
 
 	return int(intValue), bytesConsumed, nil
+}
+
+// readArray - reads array type from the input stream
+// Example input - *2\r\n$5\r\nhello\r\n$5\r\nworld\r\n
+func (r *Resp) readArray() (*Value, error) {
+	v := &Value{}
+	v.typ = "array"
+
+	// start has been read
+	arraySize, _, err := r.readInt()
+	if err != nil {
+		return nil, err
+	}
+
+	v.array = make([]Value, arraySize)
+	for range arraySize - 1 {
+		val, err := r.Read()
+		if err != nil {
+			return nil, err
+		}
+
+		if val == nil {
+			return nil, fmt.Errorf("missing value in input stream")
+		}
+
+		v.array = append(v.array, *val)
+	}
+
+	return v, nil
+}
+
+// readBulk - reads bulk type from the input stream
+func (r *Resp) readBulk() (*Value, error) {
+	v := &Value{}
+	v.typ = "bulk"
+
+	bulkSize, _, err := r.readInt() // Reads until the \r\n no need to worry about the initial one
+	if err != nil {
+		return nil, err
+	}
+
+	bulk := make([]byte, bulkSize)
+	_, err = r.reader.Read(bulk)
+	if err != nil {
+		return nil, err
+	}
+
+	// Read trailing CRLF
+	_, _, err = r.readLine()
+	if err != nil {
+		return nil, err
+	}
+
+	return v, nil
 }
