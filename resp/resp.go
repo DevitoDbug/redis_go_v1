2b55b6f93f406270c23d4bd1e1a8c6d2b4dce446
devitodbug <davidochiengy@gmail.com>
2026-04-11 19:05:27 +0300

    feat: refactor helper function to helpers.go for more sematic grouping

--- resp/resp.go
+++ resp/resp.go
@@ -6,7 +6,6 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"strconv"
 )
 
 type Resp struct {
@@ -33,51 +32,6 @@ func (r *Resp) Read() (*Value, error) {
 	default:
 		return nil, fmt.Errorf("invalid input type")
 	}
-}
-
-func (r *Resp) readLine() ([]byte, int, error) {
-	var line []byte
-
-	for {
-		b, err := r.reader.ReadByte()
-		if err != nil {
-			return nil, 0, err
-		}
-
-		if b == '\r' {
-			nextChar, err := r.reader.Peek(1)
-			if err != nil {
-				return nil, 0, err
-			}
-
-			if nextChar[0] == '\n' {
-				_, err := r.reader.Discard(1)
-				if err != nil {
-					return nil, 0, err
-				}
-
-				break
-			}
-		}
-
-		line = append(line, b)
-	}
-
-	return line, len(line) + 2, nil // +2 because we need to account for CRLF
-}
-
-func (r *Resp) readInt() (val int, bytesConsumed int, err error) {
-	line, bytesConsumed, err := r.readLine()
-	if err != nil {
-		return 0, 0, err
-	}
-
-	intValue, err := strconv.ParseInt(string(line), 10, 64)
-	if err != nil {
-		return 0, 0, err
-	}
-
-	return int(intValue), bytesConsumed, nil
 }
 
 // readArray - reads array type from the input stream
